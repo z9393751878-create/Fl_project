7177eb028d8424639e8f0a89193f6f67fde097d0
z9393751878-create <z9393751878@gmail.com>
2026-03-11 03:29:20 +1345

    app.py

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+import os
 
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',debug=True)
